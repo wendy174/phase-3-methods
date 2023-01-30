c46f9b510b6c9db85c876e4656b934a44948fc1d
wendy <mizukilee@gmail.com>
2023-01-30 16:30:36 -0500

    commit tree 8c3c83640b042b7e86fd72272659a9ad374c5bfa

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
 # Your code here!
+
+def greet_programmer
+    puts 'Hello, programmer!'
+end
+
+def greet(name)
+    puts "Hello, #{name}"
+end
+
+def greet_with_default(name = 'Hello, programmer!') 
+    puts "Hello, #{name}"
+end
+
+def add(num1, num2)
+    returns num1 + num2 
+end
+
+def halve(num) 
+    returns num / 2 
+end
